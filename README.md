343d2cc0c6d05df4ae8772c68a39882ee14ab2cf
pjshu <shushugo233@gmail.com>
2020-06-24 17:08:25 +0800

    readme

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 ****
 
+
 ![screenshot](./2.png)
+
+## build
+```bash
+yarn build GENERATE_SOURCEMAP=false
+```
+
+or
+
+```bash
+cd build/
+# open index.html
+```
+
+## dev
+```bash
+yarn start
+```
---
+++ build/precache-manifest.85adb45a57cb04e2287f8ceccd0d921d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1b6ab03afd08e6f0b6fe75fa319bd16",
+    "url": "./index.html"
+  },
+  {
+    "revision": "67f2235670e8dd3c9bf3",
+    "url": "./static/css/main.d218a688.chunk.css"
+  },
+  {
+    "revision": "33c28ea9fe35f82cd23c",
+    "url": "./static/js/2.980f7f62.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.980f7f62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67f2235670e8dd3c9bf3",
+    "url": "./static/js/main.4ec0dea7.chunk.js"
+  },
+  {
+    "revision": "c77fecb7a6b82fec87b6",
+    "url": "./static/js/runtime-main.be1df3d2.js"
+  },
+  {
+    "revision": "0e6336880befb88b59894a2c19714a74",
+    "url": "./static/media/GilPUG.0e633688.jpg"
+  },
+  {
+    "revision": "c00f94cb6bf7f0127ec4abb69baf4da1",
+    "url": "./static/media/bangpypers.c00f94cb.png"
+  },
+  {
+    "revision": "5e6ab708ba3d5dcc614d479c8dbf1b61",
+    "url": "./static/media/pyie.5e6ab708.png"
+  },
+  {
+    "revision": "3f87377807b33f418d2bd767d6a87935",
+    "url": "./static/media/pymug_new.3f873778.svg"
+  },
+  {
+    "revision": "d8c37b67ab07c2f936be36ed00ae7621",
+    "url": "./static/media/pynigeria.d8c37b67.svg"
+  },
+  {
+    "revision": "83e841ce23da1c4aa5f72c48cac79637",
+    "url": "./static/media/python_amsterdam.83e841ce.svg"
+  },
+  {
+    "revision": "4b7f4f2431ccf6d20c2af3bf1ef8e5f5",
+    "url": "./static/media/python_china.4b7f4f24.jpg"
+  }
+]);
